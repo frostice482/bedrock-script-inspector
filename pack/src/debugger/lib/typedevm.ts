d08e4448d16560d4059b8c9bdb5fb667bee82958
FrostIce482 <frostice48260@gmail.com>
2024-02-10 11:48:18 +0700

    simplify typed emitter

--- pack/src/debugger/lib/typedevm.ts
+++ pack/src/debugger/lib/typedevm.ts
@@ -1,53 +1,17 @@
 export default class TypedEventEmitter<T extends Record<string, any>> {
-    _listeners: {
-        [K in keyof T]?: {
-            list: Map<Listener<T[K]>, boolean>
-            prepend: Map<Listener<T[K]>, boolean>
-        }
-    } = Object.create(null)
+    protected _listeners: { [K in keyof T]?: Set<Listener<T[K]>> } = Object.create(null)
 
-    addEventListener<K extends keyof T>(event: K, fn: Listener<T[K]>, prepend = false, once = false) {
-        const listener = this._listeners[event] ??= {
-            list: new Map<never, never>(),
-            prepend: new Map<never, never>()
-        }
-
-        listener[prepend ? 'prepend' : 'list'].set(fn, once)
-
+    addEventListener<K extends keyof T>(event: K, fn: Listener<T[K]>) {
+        (this._listeners[event] ??= new Set).add(fn)
         return this
     }
 
     removeEventListener<K extends keyof T>(event: K, fn: Listener<T[K]>) {
-        const listener = this._listeners[event]
-        if (listener) {
-            listener.list.delete(fn)
-            listener.prepend.delete(fn)
-        }
-
-        return this
+        return this._listeners[event]?.delete(fn) ?? false
     }
 
     emit<K extends keyof T>(event: K, data: T[K]) {
-        const list = this._listeners[event]
-        if (!list) return
-
-        for (const [listener, once] of list.prepend) {
-            listener(data)
-            if (once) list.prepend.delete(data)
-        }
-
-        for (const [listener, once] of list.list) {
-            listener(data)
-            if (once) list.list.delete(data)
-        }
-    }
-
-    *getListeners<K extends keyof T>(event: K) {
-        const listener = this._listeners[event]
-        if (!listener) return
-
-        yield* listener.prepend.keys()
-        yield* listener.list.keys()
+        for (const listener of this._listeners[event] ?? []) listener(data)
     }
 }
 
