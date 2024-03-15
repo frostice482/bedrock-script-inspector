9ae672da4a862a8f1a656a68f6ebeeabd9ccdb21
FrostIce482 <frostice48260@gmail.com>
2024-03-15 20:29:20 +0700

    add dummy player test

---
+++ pack/src/debugger/gametest.ts
@@ -0,0 +1,11 @@
+import { world } from '@minecraft/server'
+import * as gt from '@minecraft/server-gametest'
+
+gt.register('debug', 'spawndummy', test => {
+    world.sendMessage('Break the bedrock to end the test')
+    test.succeedWhenBlockPresent('minecraft:air', {x: 0, y: 1, z: 0}, true)
+    test.succeedOnTick(0x7ffffffe)
+    test.spawnSimulatedPlayer({x: 2, y: 2, z: 2}, 'Dummy')
+})
+    .maxTicks(0x7fffffff)
+    .structureName('debug:test')
